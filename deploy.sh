a95fcaf39a9c357332c98173615079a33cd23bae
Pragya28 <pragyajha2812@gmail.com>
2024-11-20 17:05:38 +0530

    removed copy command

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ pnpm install
 # Build the SAPod Web
 pnpm nx build sapod-web
 
-# Copy files to public_html
-cp -r dist/apps/sapod-web/* files/public_html
+# # Copy files to public_html
+# cp -r dist/apps/sapod-web/* files/public_html
