2b06e737c9abbefaa1e227a6625ce92d5ce8298d
Pragya28 <pragyajha2812@gmail.com>
2025-03-04 17:45:48 +0530

    theme setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
+import { ThemeToggle } from "@sapod/components";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-gradient-to-r from-[#712ebf] to-[#b38be8] flex justify-center items-center">
-      <Image
-        src="coming_soon.png"
-        alt="Coming Soon Placeholder"
-        className="w-full h-full object-contain"
-        fill
-        priority
-      />
+    <div className="flex items-center justify-center h-screen">
+      <ThemeToggle />
     </div>
   );
 }
