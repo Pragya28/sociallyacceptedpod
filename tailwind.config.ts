b86c966a705e321bed2ba04069908bf4707b0b9a
Pragya28 <pragyajha2812@gmail.com>
2025-03-08 02:35:22 +0530

    tailwind and libs setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/*.{ts,tsx}', './libs/ui/src/ui/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
